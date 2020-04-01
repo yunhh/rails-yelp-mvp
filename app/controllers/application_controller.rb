ecea06cbf6ed8e5634efa48bb2df9edcb1dac043
guillaume Demasse demasseguillaume@gmail.com <demasseguillaume@gmail.com>
2020-04-01 16:54:24 +0200

    exo 2 fini avec un pb de css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-
-
-  def destroy
-    @review = Review.find(params[:id])
-    @review.destroy
-    redirect_to restaurant_path(@review.restaurant)
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,14 @@ puts "Cleaning database..."
 Restaurant.destroy_all
 
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: 'french' }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ",
+   category: 'italian' }
+dinnoom = { name: "Dinnoom", address: "10 picadily St, London E1 7JE", category: 'chinese' }
+rishnom = { name: "Rishnom", address: "3 levrette St, London F2 7JE", category: 'belgian' }
+dashroom = { name: "Dashroom", address: "6 round St, London E2 7JE", category: 'japanese' }
 
-[ dishoom, pizza_east ].each do |attributes|
+[ dishoom, pizza_east, dinnoom, rishnom, dashroom].each do |attributes|
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
 end
