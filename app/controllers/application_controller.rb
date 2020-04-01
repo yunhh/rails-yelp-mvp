3b83d7ae3dda148cc8c9db648ccd1d86b3260434
guillaume Demasse demasseguillaume@gmail.com <demasseguillaume@gmail.com>
2020-04-01 15:22:39 +0200

    model et validates rake vert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
 end
